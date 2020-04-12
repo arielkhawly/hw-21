8c5286f4a3c919c5ee6637a7d67f3f51268fd4ef
Ariel Khawly <akhawly59@gmail.com>
2020-04-11 17:55:59 -0700

    cont search page, added API links and event handlers

--- google-books/src/pages/Search.js
+++ google-books/src/pages/Search.js
@@ -6,9 +6,53 @@ class Search extends Component {
     state = {
         books: [],
         q: "",
+        message: "Search for a book"
 
     };
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
 
-   
+    getBooks = () => {
+        API.getBooks(this.state.q)
+            .then(res =>
+                this.setState({
+                    books: res.data
+                })
+            )
+            .catch(() =>
+                this.setState({
+                    books: [],
+                    message: "No Book found, try again"
+                })
+            );
+    };
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
+    };
+
+    handleBookSave = id => {
+        const book = this.state.books.find(book => book.id === id);
+        API.saveBook({
+            googleId: book.id,
+            title: book.volumeInfo.title,
+            link: book.volumeInfo.infoLink,
+            authors: book.volumeInfo.authors,
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+        }).then(() => this.getBooks());
+    };
+
+    render() {
+        return (
+            <Book />
+        )
+    }
+
 
 export default Search;
